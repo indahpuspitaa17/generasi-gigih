0cc56b8e84c6ba0ac76a56b55ed941f3a6899bf6
indahpuspita17 <indah_indahpuspita@apps.ipb.ac.id>
2021-06-12 12:06:57 +0700

    Java by Progate

---
+++ java/java_study_1/page15/Main.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args) {
+    // Tetapkan string ke variable name
+    String name = "Indah";
+    
+    // Tetapkan int ke variable age
+    int age = 20;
+    
+    // Tetapkan double ke variable height (contoh: 1.6)
+    double height = 155;
+    
+    // Tetapkan double ke variable weight (contoh: 50.0)
+    double weight = 45;
+    
+    // Kalkulasikan BMI dan tetapkan hasilnya ke variable bmi
+    double bmi = weight / height / height;
+    
+    // Cetak name, age, height, weight, dan bmi
+    System.out.println("Nama saya adalah " + name + "Saya berusia " + age + " tahun" + "Tinggi saya adalah " + height + "meter" + "Berat saya adalah " + weight + " kilogram" + "BMI saya adalah " + bmi);
+    
+  }
+}
