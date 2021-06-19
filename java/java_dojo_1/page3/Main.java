a089707070e461ec1382afbfa07ff01143f39a45
indahpuspita17 <indah_indahpuspita@apps.ipb.ac.id>
2021-06-19 17:57:29 +0700

    Java by Progate

---
+++ java/java_dojo_1/page3/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Nama depan: ");
+    
+    String firstName = scanner.next();
+    System.out.println("Nama saya adalah " + firstName + ".");
+  }
+}
