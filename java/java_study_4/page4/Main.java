e5ffa0297a8cbc751ee1367fdf84a2143126dad1
indahpuspita17 <indah_indahpuspita@apps.ipb.ac.id>
2021-08-17 17:56:04 +0700

    Java by Progate

---
+++ java/java_study_4/page4/Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    Person person1 = new Person();
+    // Panggil method `hello` menggunakan instance `person1`
+    person1.hello();
+    
+    Person person2 = new Person();
+    // Panggil method `hello` menggunakan instance `person2`
+    person2.hello();
+    
+    
+  }
+}
---
+++ java/java_study_4/page7/Person.java
@@ -0,0 +1,8 @@
+class Person {
+  public String name;
+
+  // Definisikan `constructor` untuk class `Person`
+  Person() {
+    System.out.println("Sebuah instance dari class Person telah terbentuk.");
+  }
+}
